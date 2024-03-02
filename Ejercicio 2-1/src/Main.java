322fb792086e583d269dfc40c56a7810ab1c1035
alejandrardecajal <alejandrardecajal@gmail.com>
2024-03-02 17:26:29 -0300

    Ejercicios 2-1 y 2-2

---
+++ Ejercicio 2-1/src/Main.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // crear array list
+        ArrayList<String> razas = new ArrayList<>();
+
+        // crear clase scanner para que pregunte al usuario por consola
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese raza: ");
+
+        String raza = "";
+
+        while(!raza.equals("0")) {
+            raza = sc.nextLine();
+            System.out.println("Ingrese otra raza o 0 para salir");
+            //agrego lo ingresado a la lista
+            if(!raza.equals("0")) {
+                razas.add(raza);
+            }
+        }
+
+        //Muestro el array
+        System.out.println("Listado de razas: " + razas);
+        System.out.println("Adios");
+
+        //Buscar raza ingresada
+        System.out.println("Ingrese la raza a buscar: ");
+        String busqueda = sc.nextLine();
+
+        //Por cada raza de razas, si busqueda = a raza, mostrarla.
+
+        boolean razaEncontrada = false;
+        for(String r : razas) {
+            if (busqueda.equals(r)){
+                System.out.println("La raza encontrada es: " + r);
+                razaEncontrada = true;
+                break;
+            }
+        }
+        //Fuera del bucle, si razaEncontrada = false, que imprima "Raza no encontrada"
+        if(!razaEncontrada){
+            System.out.println("Raza no encontrada");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
